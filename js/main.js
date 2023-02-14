09cbd53151a37e6a2d72968626f6baf3333feecb
Shady Elbordeny <102693868+Shady-Elbordeny@users.noreply.github.com>
2023-02-14 22:05:11 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// let x = prompt("enter letter")
+
+// for (let i = 0; i < arr.length; i++){
+//   if (x == arr[i]) {
+//     console.log(arr[i+1]);
+//   }
+// }
+//================================
+
